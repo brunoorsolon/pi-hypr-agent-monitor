9684da94938b5a179154c2fa9670d0a98919dd35
Legion <legion@puglab.im>
2026-09-08 00:06:40 +0200

    Add runnable test script

--- extensions/pi-hypr-agent-monitor/test.js
+++ extensions/pi-hypr-agent-monitor/test.js
@@ -1,5 +1,4 @@
-const assert = require('assert');
+import { strict as assert } from 'assert';
 console.log('Running tests...');
 assert.strictEqual(true, true, 'basic test passes');
 console.log('All tests passed.');
-
